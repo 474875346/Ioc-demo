65fff65cf9d7861ed61393081e1ff763c287f409
烟汐忆梦 <474875346@qq.com>
2020-05-19 17:48:57 +0800

    IOC注解反射属性判断

--- src/main/java/com/m/mvc/web/HanderChain.java
+++ src/main/java/com/m/mvc/web/HanderChain.java
@@ -9,16 +9,15 @@ import javax.servlet.http.HttpSession;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 负责具体的业务调用
  */
 public class HanderChain { // 当前访问的执行流程
 
-    // 拦截器集合
     private String name;
     private String method;
     private Class<?>[] pars;
@@ -41,18 +40,48 @@ public class HanderChain { // 当前访问的执行流程
         this.application = this.session.getServletContext();
     }
 
-    //hander本身
+    private Map<String, String[]> requestMap = new HashMap<String, String[]>();
 
-    public ModelAndView chain() {
+    /**
+     * 检查是否是文件上传
+     *
+     * @return true 是上传 false不是
+     */
+    private boolean ifFileUpload() {
+        return false;
+    }
+
+    /**
+     * 获取所有get或者post传递来的请求参数
+     */
+    private void getRequestMap() {
+        if (!this.ifFileUpload()) { // 获取非文件上传的请求参数
+            requestMap = this.request.getParameterMap(); // 获取所有的请求参数
+        } else {
+
+        }
+    }
+
+    private ModelAndView sendParametersByFileUpload() {
         ModelAndView mv = new ModelAndView();
+        return mv;
+    }
 
-        Map<String, String[]> map = this.request.getParameterMap(); // 获取所有的请求参数
-
-        // 获取要操作的hander
-        XMLApplicatContext ac = new XMLApplicatContext();
-        this.hander = ac.getBean(name);
-        // 获取要操作的方法
+    /**
+     * 往hander的方法当中，提交参数，这一部分参数，
+     * 有的来自于请求requestMap，
+     * 有的来自于服务器本身3大域+response，
+     * 有的属于框架规则ModelMapping
+     *
+     * @return
+     */
+    private ModelAndView sendParametersByNoFileUpload() {
+        ModelAndView mv = new ModelAndView();
         try {
+            // 获取要操作的hander
+            XMLApplicatContext ac = new XMLApplicatContext();
+            this.hander = ac.getBean(name);
+            // 获取要操作的方法
             Method method = this.hander.getClass().getMethod(this.method, this.pars);
             Parameter[] parameters = method.getParameters(); // 拿到所有参数
             List pars = new ArrayList(); // 要执行的参数
@@ -62,7 +91,83 @@ public class HanderChain { // 当前访问的执行流程
                 String parname = p.getName();
                 // 参数当中有可能出现的情况
                 // 3大域+response/ModelMapping/UploadFile/参数
+                if (partype.getName().equals(HttpServletRequest.class.getName())) {
+                    pars.add(this.request);
+                } else if (partype.getName().equals(HttpServletResponse.class.getName())) {
+                    pars.add(this.response);
+                } else if (partype.getName().equals(HttpSession.class.getName())) {
+                    pars.add(this.session);
+                } else if (partype.getName().equals(ServletContext.class.getName())) {
+                    pars.add(this.application);
+                } else if (partype.getName().equals(ModelMapping.class.getName())) {
+                    pars.add(model);
+                } else {
+                    if (partype.getName().equals(Byte.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Byte.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Short.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Short.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Integer.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Integer.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Long.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Long.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Float.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Float.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Double.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Double.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Boolean.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(Boolean.class.getConstructor(String.class).newInstance(requestMap.get(parname)[0]));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(String.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(requestMap.get(parname)[0]);
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(String[].class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            pars.add(requestMap.get(parname));
+                        } else {
+                            pars.add(null);
+                        }
+                    } else if (partype.getName().equals(Date.class.getName())) {
+                        if (requestMap.get(parname) != null) {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                            Date date = sdf.parse(requestMap.get(parname)[0]);
+                            pars.add(date);
+                        } else {
+                            pars.add(null);
+                        }
+                    }
+                }
 
+//                String[] parStrs = requestMap.get(parname); // 获取请求到的参数
             }
 
             method.invoke(this.hander, pars.toArray());
@@ -74,10 +179,23 @@ public class HanderChain { // 当前访问的执行流程
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
 
         return mv;
+    }
+
+    public ModelAndView chain() {
+        this.getRequestMap();// 获取所有get或者post的请求参数
+        if (this.ifFileUpload()) {
+            return this.sendParametersByFileUpload(); // 是文件上传的参数封装
+        } else {
+            return this.sendParametersByNoFileUpload(); // 不是文件上传的参数封装
+        }
     }
 
 }
